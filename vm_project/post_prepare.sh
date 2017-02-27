15dd825b83f655dbfdd6191de575aac54c057b0b
Felix Khachatrian <felix.khachatrian@gmail.com>
2017-02-27 13:42:13 +0400

    change data dir - match BOS

--- vm_project/post_prepare.sh
+++ vm_project/post_prepare.sh
@@ -9,3 +9,8 @@ curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/loca
 sudo sed -i "s/^user.*/user www-data;/" /etc/nginx/nginx.conf
 
 sudo service nginx restart
+
+# prepare data dir
+
+mkdir -p /var/data
+chown -R www-data.www-data /var/data
